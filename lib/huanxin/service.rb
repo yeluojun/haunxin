3d81dc31fc8f5239f30e1a62b46280dc763148f2
yeluojun <635143970@qq.com>
2015-09-07 19:20:51 +0800

    add add user to block

--- lib/huanxin/service.rb
+++ lib/huanxin/service.rb
@@ -1,6 +1,7 @@
 require 'rest-client'
 require 'logging'
 require 'json'
+require 'uri'
 module Huanxin
   class Service
     def initialize
@@ -18,8 +19,8 @@ module Huanxin
     # 获取token
     def get_token
       url = "#{@host}/#{@org}/#{@app}/token"
-      header = {'Content-Type': 'application/json'}
-      params =  {'grant_type': 'client_credentials', 'client_id': @client_id, 'client_secret': @client_secret}
+      header = { 'Content-Type': 'application/json' }
+      params =  { 'grant_type': 'client_credentials', 'client_id': @client_id, 'client_secret': @client_secret }
       begin
         ret = RestClient.post url, params.to_json, header
         return json(ret)
@@ -31,14 +32,14 @@ module Huanxin
     end
 
     # 注册im用户(单个用户)
-    def register(username, password, token = '')
+    def register(username, password, token = nil)
       url = "#{@host}/#{@org}/#{@app}/users"
-      if token.empty?
-        header = {'Content-Type': 'application/json'}
+      if token == nil or token == ''
+        header = { 'Content-Type': 'application/json' }
       else
-        header = {'Content-Type': 'application/json', 'Authorization': "Bearer #{token}"}
+        header = { 'Content-Type': 'application/json', 'Authorization': "Bearer #{token}" }
       end
-      params = {'username': "#{username}", "password": "#{password}"}
+      params = { 'username': "#{username}", "password": "#{password}" }
       begin
         ret = RestClient.post url, params.to_json, header
         return json(ret)
@@ -51,20 +52,173 @@ module Huanxin
 
     # 注册用户(批量)
     # 批量注册的用户数量不要过多, 建议在20-60之间
-    def register_s(users = {}, token)
-
+    # users = [{ username: , password: }] or users = { username:, password: }
+    def register_s(users = {}, token = nil)
+      url = "#{@host}/#{@org}/#{@app}/users"
+      if token == nil or token == ''
+        header = { 'Content-Type': 'application/json' }
+      else
+        header = { 'Content-Type': 'application/json', 'Authorization': "Bearer #{token}" }
+      end
+      rg_users = users
+      begin
+        ret = RestClient.post url, rg_users.to_json, header
+        return json(ret)
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
     end
 
-    # # 批量注册用户
-    # def register(user = [], token = '')
-    #   url = "#{@host}/#{@org}/#{@app}/users"
-    #   if token.blank?
-    #     header = {'Content-Type': 'application/json'}
-    #   else
-    #     header = {'Content-Type': 'application/json', 'Authorization': "Bearer $#{token}"}
-    #   end
-    # end
+    # 获取单个用户
+    def get_user(username, token)
+      url = "#{@host}/#{@org}/#{@app}/users/#{username}"
+      header = { 'Authorization': "Bearer #{token}", accept: :json }
+      begin
+        ret = RestClient.get url, header
+        return json(ret)
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
 
+    # 批量获取用户
+    def get_users(token, limit = 10, cursor = nil)
+      url = "#{@host}/#{@org}/#{@app}/users"
+      if cursor == nil or cursor == nil
+        params = {params: {limit: limit}, Authorization: "Bearer #{token}", accept: :json}
+      else
+        params = {params: {limit: limit, cursor: cursor}, Authorization: "Bearer #{token}", accept: :json}
+      end
+      begin
+        ret = RestClient.get(url, params)
+        return json(ret)
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
+
+    # 删除单个用户
+    def delete_user(username, token)
+      url = "#{@host}/#{@org}/#{@app}/users/#{username}"
+      header = { 'Authorization': "Bearer #{token}", accept: :json }
+      begin
+        ret = RestClient.delete url, header
+        return json ret
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
+
+    # 批量删除用户
+    # 删除哪些用户是不是道的，要出response,坑爹的环信
+    # 默认删除2个用户
+    def delete_users(token, limit = 2)
+      url = "#{@host}/#{@org}/#{@app}/users"
+      header = {params: {limit: limit}, Authorization: "Bearer #{token}", accept: :json }
+      begin
+        ret = RestClient.delete url, header
+        return json ret
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
+
+    # 重置IM用户密码
+    def reset_password(token, username, password)
+      url = "#{@host}/#{@org}/#{@app}/users/#{username}/password"
+      header = { Authorization: "Bearer #{token}", accept: :json }
+      begin
+        ret = RestClient.put url, {newpassword: password}.to_json, header
+        return json ret
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
+
+    # 修改用户昵称
+    def set_nickname(token, username, nickname)
+      url = "#{@host}/#{@org}/#{@app}/users/#{username}"
+      header = { Authorization: "Bearer #{token}", accept: :json }
+      begin
+        ret = RestClient.put url, {nickname: nickname}.to_json, header
+        return json ret
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
+
+    # 给IM用户添加朋友
+    # owner_username=>自己; friend_username=>朋友
+    def add_friend(token, owner_username, friend_username)
+      url = "#{@host}/#{@org}/#{@app}/users/#{owner_username}/contacts/users/#{friend_username}"
+      header = { Authorization: "Bearer #{token}", accept: :json }
+      begin
+        ret = RestClient.post url, {}.to_json, header
+        return json ret
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
+
+    # 从IM用户的好友列表中移除一个用户
+    def remove_friend(token, owner_username, friend_username )
+      url = "#{@host}/#{@org}/#{@app}/users/#{owner_username}/contacts/users/#{friend_username}"
+      header = { Authorization: "Bearer #{token}", accept: :json }
+      begin
+        ret = RestClient.delete url, header
+        return json ret
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
+
+    # 查看某个IM用户的好友信息
+    def get_friends(token, username)
+      url = "#{@host}/#{@org}/#{@app}/users/#{username}/contacts/users"
+      header = { Authorization: "Bearer #{token}", accept: :json }
+      begin
+        ret = RestClient.get url, header
+        return json ret
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
+
+    # 往一个IM用户的黑名单中加人(可以多个)
+    # users可以为一个也可以多个
+    def block_add_users(token, username, users)
+      url = "#{@host}/#{@org}/#{@app}/users/#{username}/blocks/users"
+      header = { Authorization: "Bearer #{token}", accept: :json }
+      params = {usernames: users}
+      begin
+        ret = RestClient.post url, params.to_json, header
+        return json ret
+      rescue => ex
+        $logger.error(ex.response.inspect)
+        ret = ex.response
+        return json(ret)
+      end
+    end
 
     private
 
@@ -74,7 +228,8 @@ module Huanxin
       return res
     end
 
-
-
+    def url_encode(str)
+      return str.gsub!(/[^\w$&\-+.,\/:;=?@]/) { |x| x = format("%%%x", x[0]) }
+    end
   end
 end
--- spec/huanxin/service_spec.rb
+++ spec/huanxin/service_spec.rb
@@ -15,8 +15,9 @@ describe Huanxin do
       config.log_file = "#{Huanxin.root}/log/huanxin.log"
       config.log_level = @deault_config[:log_level].to_sym
     end
-    p  Huanxin.config
     @service = Huanxin::Service.new
+    t = @service.get_token
+    @token = t['access_token']
   end
 
   # get token test
@@ -26,18 +27,219 @@ describe Huanxin do
   end
 
   # 注册用户测试
-  context 'POST#register users' do
-    it 'register users success' do
-      ret = @service.get_token
-      ret = @service.register('yeluo', '12345678', ret['access_token'])
+  context 'POST#register' do
+    it 'register user success' do
+      ret = @service.register('yeluo', '12345678', @token)
       expect(ret['code']).to eq 200
     end
 
     it 'register fail when username is not allow' do
-      ret = @service.get_token
-      ret = @service.register('修磊狮虎', '12345678', ret['access_token'])
+      ret = @service.register('修磊狮虎', '12345678', @token)
       expect(ret['code']).to eq 400
     end
   end
+
+  # 批量注册用户测试
+  context 'POST#register_s' do
+    it 'should register one user' do
+      ret = @service.register_s({ username: 'test2', password: '123456' }, @token)
+      p ret
+      expect(ret['code']).to eq 200
+    end
+
+    it 'should register two users' do
+      ret = @service.register_s([{ username: 'test3', password: '123456' },
+                                 { username: 'test4', password: '123456' }], @token)
+      p ret
+      expect(ret['code']).to eq 200
+    end
+  end
+
+  # 获取单个用户
+  context 'GET#get_user' do
+    before do
+      @service.register_s({ username: 'y_get_user', password: '123456' }, @token)
+    end
+    it 'should get one user' do
+      ret = @service.get_user('y_get_user', @token)
+      p ret
+      expect(ret['code']).to eq 200
+    end
+
+    it 'should get 404' do
+      ret = @service.get_user('no_name', @token)
+      p ret
+      expect(ret['code']).to eq 404
+    end
+
+    it 'should get 401' do
+      ret = @service.get_user('y_get_user', '')
+      p ret
+      expect(ret['code']).to eq 401
+    end
+  end
+
+  # 批量获取用户
+  context  'GET#get_users' do
+    before do
+      @service.register_s([{ username: 'get_users_1', password: '123456' },
+                           { username: 'get_users_2', password: '123456' },
+                           { username: 'get_users_3', password: '123456' },
+                           { username: 'get_users_4', password: '123456' },
+                           { username: 'get_users_5', password: '123456' },
+                           { username: 'get_users_6', password: '123456' },
+                           { username: 'get_users_7', password: '123456' },
+                           { username: 'get_users_8', password: '123456' },
+                           { username: 'get_users_9', password: '123456' },
+                           { username: 'get_users_10', password: '123456' },
+                           { username: 'get_users_11', password: '123456' },
+                           { username: 'get_users_12', password: '123456' },
+                           { username: 'get_users_13', password: '123456' },
+                           { username: 'get_users_14', password: '123456' },
+                           { username: 'get_users_15', password: '123456' },
+                           { username: 'get_users_16', password: '123456' },
+                           { username: 'get_users_17', password: '123456' },
+                           { username: 'get_users_18', password: '123456' },
+                           { username: 'get_users_19', password: '123456' },
+                           { username: 'get_users_20', password: '123456' },
+                           { username: 'get_users_21', password: '123456' },
+                           { username: 'get_users_22', password: '123456' },
+                           { username: 'get_users_23', password: '123456' },
+                           { username: 'get_users_24', password: '123456' },
+                           { username: 'get_users_25', password: '123456' },
+                           { username: 'get_users_26', password: '123456' },
+                           { username: 'get_users_27', password: '123456' },
+                           { username: 'get_users_28', password: '123456' },
+                           { username: 'get_users_29', password: '123456' },
+                           { username: 'get_users_30', password: '123456' },
+                           { username: 'get_users_31', password: '123456' },
+                           { username: 'get_users_32', password: '123456' },
+                           { username: 'get_users_33', password: '123456' },
+                           { username: 'get_users_34', password: '123456' },
+                           { username: 'get_users_35', password: '123456'}], @token)
+    end
+    it 'should get two users' do
+      ret = @service.get_users(@token, 30)
+      expect(ret['code']).to eq 200
+      expect(ret['entities'].length).to eq 30
+      cursor = ret['cursor']
+
+      # 获取下一页
+      ret = @service.get_users(@token, 30, cursor)
+      expect(ret['code']).to eq 200
+      expect(ret['entities'].length).to eq 5
+    end
+  end
+
+  # 测试删除用户
+  context 'DELETE#delete_user' do
+    before do
+      @service.register('nimabi', '123456', @token)
+    end
+    it 'should delete a user named nimabi' do
+      ret = @service.delete_user('nimabi', @token)
+      p ret
+      expect(ret['code']).to eq 200
+    end
+    it 'should be 404' do
+      ret = @service.delete_user('no_this_user', @token)
+      expect(ret['code']).to eq 404
+    end
+  end
+
+  # 测试批量删除用户
+  context  'DELETE#delete_users' do
+    before do
+      @service.register('delete_users_1', '123456', @token)
+      @service.register('delete_users_2', '123456', @token)
+    end
+    it 'should delete two users' do
+      ret = @service.delete_users @token, 2
+      p ret
+      expect(ret['code']).to eq 200
+    end
+  end
+
+  # 测试重置用户密码
+  context  'PUT#rest_password' do
+    before do
+      @service.register('rest_password_1', '123456', @token)
+    end
+    it 'should rest password' do
+      ret = @service.reset_password @token, 'rest_password_1', '1234567'
+      p ret
+      expect(ret['code']).to eq 200
+    end
+  end
+
+  # 测试修改用户昵称
+  context  'PUT#set_nickname' do
+    before do
+      @service.register('set_nickname', '123456', @token)
+    end
+    it 'should rest password' do
+      ret = @service.set_nickname @token, 'set_nickname', '雷洪'
+      p ret
+      expect(ret['code']).to eq 200
+    end
+  end
+
+  context  'POST#add_friend' do
+    before do
+      @service.register('add_friend_owner', '123456', @token)
+      @service.register('add_friend_friend', '123456', @token)
+    end
+    it 'should rest password' do
+      ret = @service.add_friend @token, 'add_friend_owner', 'add_friend_friend'
+      p ret
+      expect(ret['code']).to eq 200
+    end
+  end
+
+  context  'POST#remove_friend' do
+    before do
+      @service.register('remove_friend_owner', '123456', @token)
+      @service.register('remove_friend_friend', '123456', @token)
+      @service.add_friend @token, 'remove_friend_owner', 'remove_friend_friend'
+    end
+    it 'should rest password' do
+      ret = @service.remove_friend @token, 'remove_friend_owner', 'remove_friend_friend'
+      p ret
+      expect(ret['code']).to eq 200
+    end
+  end
+
+  context  'GET#get_friends' do
+    before do
+      @service.register('remove_friend_owner', '123456', @token)
+      @service.register('remove_friend_friend', '123456', @token)
+      @service.register('remove_friend_friend_2', '123456', @token)
+      @service.add_friend @token, 'remove_friend_owner', 'remove_friend_friend'
+      @service.add_friend @token, 'remove_friend_owner', 'remove_friend_friend_2'
+    end
+    it 'should rest password' do
+      ret = @service.get_friends @token, 'remove_friend_owner'
+      p ret
+      expect(ret['code']).to eq 200
+      expect(ret['data'].length).to eq 2
+    end
+  end
+
+  context 'POST#blocks_add_users' do
+    before do
+      @service.register('block_friend_owner', '123456', @token)
+      @service.register('block_friend_friend', '123456', @token)
+      @service.register('block_friend_friend_2', '123456', @token)
+      @service.add_friend @token, 'block_friend_owner', 'block_friend_friend'
+      @service.add_friend @token, 'block_friend_owner', 'block_friend_friend_2'
+    end
+    it 'should rest password' do
+      ret = @service.block_add_users @token, 'remove_friend_owner'
+      p ret
+      expect(ret['code']).to eq 200
+      expect(ret['data'].length).to eq 2
+    end
+  end
+
 
 end
